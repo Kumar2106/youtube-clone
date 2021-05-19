7323ea8efd08e9bcfe95d0d9852c7391356ba437
Kumar2106 <ka09934147002@gmail.com>
2021-05-19 22:03:31 +0530

    initial stage of the youtube clone

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,10 @@
+/** @format */
+
+const firebaseConfig = {
+	apiKey: "AIzaSyByISF97oOFAQdJCsDR9FbgQfkU3uVrtQo",
+	authDomain: "clone-1d9bb.firebaseapp.com",
+	projectId: "clone-1d9bb",
+	storageBucket: "clone-1d9bb.appspot.com",
+	messagingSenderId: "167177560721",
+	appId: "1:167177560721:web:fc3d04086765f74ce49530",
+};
